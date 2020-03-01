720ee64715d276094137e8c08f9d7bb88f01470d
boydfd <boydfd@163.com>
2020-03-01 11:14:04 +0800

    [@boydfd] #N/A throw exception when limit reached.

--- src/main/java/com/aboydfd/parkinglot/ParkingLot.java
+++ src/main/java/com/aboydfd/parkinglot/ParkingLot.java
@@ -4,9 +4,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ParkingLot {
+    private final int limit;
     Set<Car> parkedCars = new HashSet<>();
+
+    public ParkingLot(int limit) {
+        this.limit = limit;
+    }
+
     public void park(Car car) {
+        limitationCheck();
         parkedCars.add(car);
+    }
+
+    private void limitationCheck() {
+        if (limit <= parkedCars.size()) {
+            throw new MaxCarLimitReachedException();
+        }
     }
 
     boolean isExist(Car car) {
--- src/test/java/com/aboydfd/parkinglot/ParkingLotTest.java
+++ src/test/java/com/aboydfd/parkinglot/ParkingLotTest.java
@@ -1,5 +1,6 @@
 package com.aboydfd.parkinglot;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +10,18 @@ class ParkingLotTest {
     void limit_is_not_reach___park_a_car___car_added() {
         Car car = new Car("1");
         Car car2 = new Car("1");
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         assertFalse(parkingLot.isExist(car2));
         parkingLot.park(car);
         assertTrue(parkingLot.isExist(car2));
+    }
+
+    @Test
+    void parking_lot_reaching_limit_parking_car_number___park_a_car___throw_limit_reach_exception() {
+        Car car = new Car("1");
+        Car car2 = new Car("2");
+        ParkingLot parkingLot = new ParkingLot(1);
+        parkingLot.park(car);
+        assertThrows(MaxCarLimitReachedException.class, () -> parkingLot.park(car2));
     }
 }
